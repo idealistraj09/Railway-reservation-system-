b33c6d9aea0f701c04d403155bb67632ee417373
idealistraj09 <96573598+idealistraj09@users.noreply.github.com>
2021-12-30 15:29:59 +0530

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -1,8 +1,8 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="signin.css">
-    <script src="signin.js"></script>
+    <link rel="stylesheet" href="css/signin.css">
+    <script src="js/signin.js"></script>
     <title>sign up page</title>
 </head>
 
